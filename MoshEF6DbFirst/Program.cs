6f10fd753be7718e062b8a4f1e1809c4ff503642
Raman Thatikunta <raman_k_t@hotmail.com>
2020-06-27 18:59:43 -0400

    Add project files.

---
+++ MoshEF6DbFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoshEF6DbFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dbContext = new VidzyDbContext();
+            var releaseDateTime = new DateTime(2020, 6, 27);
+            dbContext.AddVideo("Video81", releaseDateTime.AddDays(1), 1, (byte) Classification.Platinum);
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+}
